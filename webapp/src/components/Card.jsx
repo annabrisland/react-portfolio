967ea6e2d494ebafb003c85199e9db1912de0210
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-01-08 20:42:01 +0000

    Adds project links & images

--- webapp/src/components/Card.jsx
+++ webapp/src/components/Card.jsx
@@ -6,10 +6,14 @@ function Card(props) {
 
     return (
         <div className="card">
-            <img className="proj-img" src={project.img}></img>
+            <img className="proj-img" src={project.img} style={{width:"800px"}}></img>
             <div className="info">
-                <div className="title">{project.title}</div>
+                <h3 className="title">{project.title}</h3>
                 <div className="description">{project.desc}</div>
+                <div className="project-links">
+                    <button className="github-btn" href={project.github}>GitHub</button>
+                    <button className="view-btn" href={project.deployed}>View</button>
+                </div>
             </div>
         </div>
     );
--- webapp/src/components/pages/Projects.jsx
+++ webapp/src/components/pages/Projects.jsx
@@ -1,25 +1,62 @@
 import React from "react";
 import Card from "../Card";
 
+import README from "../../assets/README.png";
+import noFomo from "../../assets/noFomo.png";
+import weather from "../../assets/weather.png";
+import labby from "../../assets/LabbyIntro.png";
+import umami from "../../assets/Umami.png";
+import botany from "../../assets/BotanyIntro.png";
+
 function Projects() {
   const project = [
     {
+      id: 0,
+      title: "README Generator",
+      desc: "Generate a great README quickly for any project",
+      img: README,
+      github: "https://github.com/annabrisland/create-a-README",
+      deployed: "https://github.com/annabrisland/create-a-README",
+    },
+    {
       id: 1,
-      title: "Labby",
-      desc: "A Lab Management System for BC Cancer",
-      img: "",
+      title: "No FOMO",
+      desc: "A stuff-to-do planner",
+      img: noFomo,
+      github: "https://github.com/kevinstaresdarbon/NoFOMO",
+      deployed: "https://kevinstaresdarbon.github.io/NoFOMO/",
     },
     {
       id: 2,
-      title: "Umami",
-      desc: "Recipe Sharing Platform",
-      img: "",
+      title: "Weather Dash",
+      desc: "A weather dashboard for your chosen location",
+      img: weather,
+      github: "https://github.com/annabrisland/weather-dash",
+      deployed: "https://annabrisland.github.io/weather-dash/",
     },
     {
       id: 3,
+      title: "Labby",
+      desc: "A Lab Management System for BC Cancer",
+      img: labby,
+      github: "https://github.com/ubclaunchpad/labby",
+      deployed: "https://annabrisland.com/#/labby/",
+    },
+    {
+      id: 4,
+      title: "Umami",
+      desc: "Recipe Sharing Platform",
+      img: umami,
+      github: "https://github.com/ubclaunchpad/umami",
+      deployed: "https://annabrisland.com/#/umami/",
+    },
+    {
+      id: 5,
       title: "UBC Botany",
       desc: "Merch and Mascot Design",
-      img: "",
+      img: botany,
+      github: "https://annabrisland.com/#/ubcbotany/",
+      deployed: "https://annabrisland.com/#/ubcbotany/",
     },
   ];
 
@@ -29,6 +66,9 @@ function Projects() {
       <Card project={project[0]} />
       <Card project={project[1]} />
       <Card project={project[2]} />
+      <Card project={project[3]} />
+      <Card project={project[4]} />
+      <Card project={project[5]} />
     </div>
   );
 }
