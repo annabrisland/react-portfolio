b7368902033867e31f50007f7ec3dead74844390
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-01-09 15:39:16 +0000

    Adds responsive styling

--- webapp/src/components/pages/Home.jsx
+++ webapp/src/components/pages/Home.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import avatar from "../../assets/avatar.png";
 
 function Home() {
   // Change background colour for home page
   document.body.style.backgroundColor = "#4901ff";
+  //   Change document title
+  document.title = "Anna Brisland";
 
   return (
     <div className="name-container">
-      <h1 className="name">ANNA</h1>
+      <div className="first-name">
+        <h1 className="name">ANNA</h1>
+        <img src={avatar} style={{ width: "70px" }}></img>
+      </div>
       <h1 className="name">BRISLAND</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-        quae laborum.
-      </p>
+      <h2>WEB DESIGNER & DEVELOPER</h2>
     </div>
   );
 }
